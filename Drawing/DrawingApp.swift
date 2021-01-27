78a1c20a516b8da4d7495a7b33355a334e8fd845
Scott Obara <scott@rubbscreations.com>
2021-01-27 20:28:02 +1100

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Scott Obara on 27/1/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
