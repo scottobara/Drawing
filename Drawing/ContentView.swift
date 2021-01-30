0156048f7502a5554699e061021e01e3ee266141
Scott Obara <scott@rubbscreations.com>
2021-01-30 15:26:05 +1100

    ColorCyclingArrow

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,59 +7,107 @@
 
 import SwiftUI
 
-struct Checkerboard: Shape {
-    var rows: Int
-    var columns: Int
+struct Triangle: InsettableShape {
+    var insetAmount : CGFloat = 0
     
-    public var animatableData: AnimatablePair<Double, Double> {
-        get {
-           AnimatablePair(Double(rows), Double(columns))
-        }
-
-        set {
-            self.rows = Int(newValue.first)
-            self.columns = Int(newValue.second)
-        }
-    }
-
     func path(in rect: CGRect) -> Path {
         var path = Path()
+        
+        path.move(to: CGPoint(
+                        x: rect.midX,
+                        y: rect.minY + (insetAmount > rect.midY ? rect.midY : insetAmount))
+        )
+        path.addLine(to: CGPoint(
+                        x: rect.minX + (insetAmount > rect.midX ? rect.midX : insetAmount),
+                        y: rect.maxY - (insetAmount > rect.midY ? rect.midY : insetAmount))
+        )
+        path.addLine(to: CGPoint(
+                        x: rect.maxX - (insetAmount > rect.midX ? rect.midX : insetAmount),
+                        y: rect.maxY - (insetAmount > rect.midY ? rect.midY : insetAmount))
+        )
+        path.addLine(to: CGPoint(
+                        x: rect.midX,
+                        y: rect.minY + (insetAmount > rect.midY ? rect.midY : insetAmount))
+        )
 
-        // figure out how big each row/column needs to be
-        let rowSize = rect.height / CGFloat(rows)
-        let columnSize = rect.width / CGFloat(columns)
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var triangle = self
+        triangle.insetAmount += amount
+        return triangle
+    }
+}
 
-        // loop over all rows and columns, making alternating squares colored
-        for row in 0..<rows {
-            for column in 0..<columns {
-                if (row + column).isMultiple(of: 2) {
-                    // this square should be colored; add a rectangle here
-                    let startX = columnSize * CGFloat(column)
-                    let startY = rowSize * CGFloat(row)
+struct ColorCyclingArrow: View {
+    var amount = 0.0
+    var steps = 100
+    var thickness: CGFloat = 1.0
 
-                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
-                    path.addRect(rect)
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                VStack(spacing: 0.0) {
+                    Triangle()
+                        .inset(by: CGFloat(value))
+                        .strokeBorder(self.color(for: value, brightness: 1), lineWidth: 2)
+                        .frame(width: 200 * thickness, height: 200 * thickness)     ///Change width back to 200 unless inset is fixxed
+                    Rectangle()
+                        .inset(by: CGFloat(value))
+                        .strokeBorder(self.color(for: value, brightness: 1), lineWidth: 2)
+                        .frame(width: 100 * thickness, height: 300 * thickness)
                 }
             }
         }
+    }
 
-        return path
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
 struct ContentView: View {
-    @State private var rows = 4
-    @State private var columns = 4
+    
+    @State var thickness: CGFloat = 1.0
+    @State var colorCycle: CGFloat = 1.0
+    @State var amount = 0.5
+    
     
     var body: some View {
-        Checkerboard(rows: rows, columns: columns)
-            .onTapGesture {
-                withAnimation(.linear(duration: 3)) {
-                    self.rows = 8
-                    self.columns = 16
-                }
+        VStack {
+            VStack(spacing: 0.0) {
+//                Triangle()
+//                    .fill(Color.blue)
+//                    .frame(width: 200 * thickness, height: 200 * thickness)
+                ColorCyclingArrow(amount: amount, thickness: thickness)
+                    //.fill(Color.blue)
+                    .frame(width: 100 * thickness, height: 300 * thickness)
             }
+            .frame(width: .infinity, height: 500)
+            
+            VStack {
+                Text("Size")
+                Slider(value: $thickness, in: 0.0...2.0)
+                Text("Colour")
+                Slider(value: $amount)
+                                
+            }
+            .padding(.horizontal)
+            .padding()
+            .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 0.5)))
+            
+        }
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
